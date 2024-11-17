994d7fc7ff1e7ffde52606dcee585064a6370d5a
Ebenezer <ebenezeraikins74@gmail.com>
2024-11-17 23:00:18 +0000

    small changes

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function UserProfile(props) {
     return(
-        <div>
+        <div style={{border: '1px solid gray', padding: '10px', margin: '10px'}}>
             <h2 style={{color: 'blue'}}>{props.name}</h2>
             <p>Age: <span style={{fontWeight: 'bold'}}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
