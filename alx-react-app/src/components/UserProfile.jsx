6ef6b856c9e74cb72ca7f4d6447d086edf080f6b
Ebenezer <ebenezeraikins74@gmail.com>
2024-11-17 19:26:34 +0000

    added syles to components

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -2,8 +2,8 @@ import React from "react";
 function UserProfile(props) {
     return(
         <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
+            <h2 style={{color: 'blue'}}>{props.name}</h2>
+            <p>Age: <span style={{fontWeight: 'bold'}}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
 
         </div>
